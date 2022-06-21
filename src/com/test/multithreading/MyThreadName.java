2c2e00538b2578e7f7670d6efed689295e5ac89d
MAC <MAC@localhost>
2022-06-21 19:49:31 +0530

    Initial Commit

--- src/com/test/multithreading/MyThreadName.java
+++ src/com/test/multithreading/MyThreadName.java
@@ -11,13 +11,16 @@ public class MyThreadName {
 	public static void main(String[] args) {
 
 		Runnable r = () -> {
-			System.out.println("Name of the Thread >>>> " + Thread.currentThread().getName());
+			for (int i = 1; i <= 5; i++) {
+
+				System.out.println("Name of the Thread >>>> " + Thread.currentThread().getName());
+			}
 		};
 
 		Thread t1 = new Thread(r);
-		t1.setName("Thread 1");
+		t1.setName("Thread A");
 		Thread t2 = new Thread(r);
-		t2.setName("Thread 2");
+		t2.setName("Thread B");
 
 		t1.start();
 		t2.start();
